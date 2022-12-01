8ce7d6fca45e93cc00a535a2aace26ef68f44f22
David <davidamir03@gmail.com>
2022-12-01 16:34:29 +0100

    day1_2

--- Day1/main.go
+++ Day1/main.go
@@ -21,17 +21,26 @@ func main() {
 	fileScanner.Split(bufio.ScanLines)
 
 	var maximum int
+	var secondM int
+	var thirdM int
 	var prevMax int
 	for fileScanner.Scan() {
 		number, _ := strconv.Atoi(fileScanner.Text())
 		prevMax += number
 		if strings.TrimRight(fileScanner.Text(), "\n") == "" {
 			if prevMax > maximum {
+				thirdM = secondM
+				secondM = maximum
 				maximum = prevMax
+			} else if prevMax > secondM {
+				thirdM = secondM
+				secondM = prevMax
+			} else if prevMax > thirdM {
+				thirdM = prevMax
 			}
 			prevMax = 0
 		}
 	}
 	readFile.Close()
-	fmt.Println(maximum)
+	fmt.Println(maximum + secondM + thirdM)
 }
